a024695e3d543c39a5c13ca74953c61a4119c9df
Ashley Schauer <aelschauer@gmail.com>
2021-01-13 22:45:05 -0700

    rename edit page

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,8 +1,7 @@
 import BrandIndexPage from "./pages/Brand/IndexPage";
 import BrandShowPage from "./pages/Brand/ShowPage";
 import BrowsePage from "./pages/BrowsePage";
-import EditPage from "./pages/Admin/EditPage";
-import MutationPage from "./pages/Admin/EditPage/MutationPage";
+import MutationPage from "./pages/Admin/MutationPage";
 import DiaperTypesPage from "./pages/DiaperTypesPage";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
@@ -10,25 +9,13 @@ import ProductPage from "./pages/ProductPage";
 export default [
   {
     path: "/admin/product-data/create",
-    title: "Brand Create Page",
-    includeInNavBar: false,
-    component: EditPage,
-  },
-  {
-    path: "/admin/diaper-data/create",
-    title: "Brand Create Page",
+    title: "Product Data Create Page",
     includeInNavBar: false,
     component: MutationPage,
   },
   {
     path: "/admin/product-data/update/:brandName",
-    title: "Brand Update Page",
-    includeInNavBar: false,
-    component: EditPage,
-  },
-  {
-    path: "/admin/diaper-data/update/:brandName",
-    title: "Brand Update Page",
+    title: "Product Data Update Page",
     includeInNavBar: false,
     component: MutationPage,
   },
